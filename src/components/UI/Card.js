f5e7bb19275b65c1471d7b9a692b47644537fb96
Lucas  de Oliveira Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2021-09-06 21:22:50 -0300

    140 - Managing Cart and Modal State

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,7 @@
-import classes from './Card.module.scss';
+import classes from "./Card.module.scss";
 
 const Card = (props) => {
-    return (
-        <div className={classes.card}>
-            {props.children}
-        </div>
-    )
-}
+  return <div className={classes.card}>{props.children}</div>;
+};
 
 export default Card;
